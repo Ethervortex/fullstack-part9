163094f59be45a4466ca8282ebf3de483fbb09a3
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-07-29 12:16:12 +0300

    Ex9.10

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagRouter from './routes/diagnoses';
 const app = express();
 import cors from 'cors';
 
@@ -12,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
