6b5e2c6001dd116ec408ff9e3c50aae7028d2295
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-07-30 15:05:12 +0300

    Ex9.19

--- diary-front/src/App.tsx
+++ diary-front/src/App.tsx
@@ -19,6 +19,120 @@ const Entries = ({ parts }: { parts: Diary[] }) => (
   </div>
 );
 
+interface EntryProps {
+  newDiary: NewDiary;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  diaryCreation: (event: React.SyntheticEvent) => void;
+}
+
+const EntryForm = ({ newDiary, handleChange, diaryCreation }: EntryProps) => (
+  <form onSubmit={diaryCreation}>
+    <div>
+      date: <input type="date" name="date" value={newDiary.date} onChange={handleChange} required />
+    </div>
+    <div>
+      visibility:
+        <label>
+          <input
+            type="radio"
+            name="visibility"
+            value="great"
+            checked={newDiary.visibility === 'great'}
+            onChange={handleChange}
+          />
+          great
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="visibility"
+            value="good"
+            checked={newDiary.visibility === 'good'}
+            onChange={handleChange}
+          />
+          good
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="visibility"
+            value="ok"
+            checked={newDiary.visibility === 'ok'}
+            onChange={handleChange}
+          />
+          ok
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="visibility"
+            value="poor"
+            checked={newDiary.visibility === 'poor'}
+            onChange={handleChange}
+          />
+          poor
+        </label>
+    </div>
+    <div>
+      weather:
+        <label>
+          <input
+            type="radio"
+            name="weather"
+            value="sunny"
+            checked={newDiary.weather === 'sunny'}
+            onChange={handleChange}
+          />
+          sunny
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="weather"
+            value="rainy"
+            checked={newDiary.weather === 'rainy'}
+            onChange={handleChange}
+          />
+          rainy
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="weather"
+            value="stormy"
+            checked={newDiary.weather === 'stormy'}
+            onChange={handleChange}
+          />
+          stormy
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="weather"
+            value="cloudy"
+            checked={newDiary.weather === 'cloudy'}
+            onChange={handleChange}
+          />
+          cloudy
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="weather"
+            value="windy"
+            checked={newDiary.weather === 'windy'}
+            onChange={handleChange}
+          />
+          windy
+        </label>
+    </div>
+    <div>
+      comment: <input type="text" name="comment" value={newDiary.comment} onChange={handleChange} />
+    </div>
+    <button type="submit">add</button>
+  </form>
+)
+
 const App = () => {
   const diaryHeader = "Diary entries";
   const newEntryHeader = "Add new entry";
@@ -60,21 +174,11 @@ const App = () => {
     <div>
       <Header name={newEntryHeader} />
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={diaryCreation}>
-        <div>
-          date: <input type="text" name="date" value={newDiary.date} onChange={handleChange} required />
-        </div>
-        <div>
-          visibility: <input type="text" name="visibility" value={newDiary.visibility} onChange={handleChange} required />
-        </div>
-        <div>
-          weather: <input type="text" name="weather" value={newDiary.weather} onChange={handleChange} required />
-        </div>
-        <div>
-          comment: <input type="text" name="comment" value={newDiary.comment} onChange={handleChange} />
-        </div>
-        <button type="submit">add</button>
-      </form>
+      <EntryForm
+        newDiary={newDiary}
+        handleChange={handleChange}
+        diaryCreation={diaryCreation}
+      />
       <Header name={diaryHeader} />
       <Entries parts={diaries} />
     </div>
